cedec58258e4411ad080b48629ee794180f24688
Michael Baumert <michael_baumert@gmx.de>
2022-06-05 11:54:41 +0200

    :lipstick: Updated container notice icons

--- src/nodes/Notice.tsx
+++ src/nodes/Notice.tsx
@@ -48,7 +48,7 @@ export default class Notice extends Node {
           }),
         },
       ],
-      toDOM: node => {
+      toDOM: (node) => {
         const select = document.createElement("select");
         select.addEventListener("change", this.handleStyleChange);
 
@@ -63,11 +63,49 @@ export default class Notice extends Node {
         let component;
 
         if (node.attrs.style === "tip") {
-          component = <StarredIcon color="currentColor" />;
+          // component = <StarredIcon color="currentColor" />;
+          component = (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style={{ width: "20px", height: "20px", marginTop: "4px" }}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+          );
         } else if (node.attrs.style === "warning") {
-          component = <WarningIcon color="currentColor" />;
+          // component = <WarningIcon color="currentColor" />;
+          component = (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style={{ width: "20px", height: "20px", marginTop: "4px" }}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          );
         } else {
-          component = <InfoIcon color="currentColor" />;
+          // component = <InfoIcon color="currentColor" />;
+          component = (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style={{ width: "20px", height: "20px", marginTop: "4px" }}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clipRule="evenodd"
+              />
+            </svg>
+          );
         }
 
         const icon = document.createElement("div");
@@ -86,10 +124,10 @@ export default class Notice extends Node {
   }
 
   commands({ type }) {
-    return attrs => toggleWrap(type, attrs);
+    return (attrs) => toggleWrap(type, attrs);
   }
 
-  handleStyleChange = event => {
+  handleStyleChange = (event) => {
     const { view } = this.editor;
     const { tr } = view.state;
     const element = event.target;
@@ -119,7 +157,7 @@ export default class Notice extends Node {
   parseMarkdown() {
     return {
       block: "container_notice",
-      getAttrs: tok => ({ style: tok.info }),
+      getAttrs: (tok) => ({ style: tok.info }),
     };
   }
 }
